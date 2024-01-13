f7cddeb1dffec0da28d8972ded8f29291b35e32c
matheusdmlopes <matheusdemarcolopes@gmail.com>
2024-01-13 17:56:40 -0300

    tentando arrumar o req01

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,36 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class TeamModel extends Model<InferAttributes<TeamModel>,
+InferCreationAttributes<TeamModel>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+TeamModel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
+
+export default TeamModel;
